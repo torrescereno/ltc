80defcdd18b6e9e21d5609bbe5c5a321ba4a2aca
Luis Torres <torrescereno@gmail.com>
2024-08-17 23:30:25 -0400

    add notes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,25 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "night-owl" },
+    }),
+  ],
   output: "server",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
     },
   }),
+  redirects: {
+    "/": "/notes",
+  },
 });
-
