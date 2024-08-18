f7a0b01ec023c2d14e2895ba95525b397ae9efae
Luis Torres <torrescereno@gmail.com>
2024-08-18 12:47:00 -0400

    remove cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     tailwind(),
     mdx({
       syntaxHighlight: "shiki",
-      shikiConfig: { theme: "night-owl" },
+      shikiConfig: { theme: "dracula" },
     }),
   ],
   output: "server",
