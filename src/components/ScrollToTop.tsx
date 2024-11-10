2d9239d8fc2f13838df34a2b7007f08ccea5fdf5
Luis Torres <torrescereno@gmail.com>
2024-11-10 14:35:53 -0300

    add arrowup

---
+++ src/components/ScrollToTop.tsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "preact/hooks";
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleScroll = () => {
+    setIsVisible(window.scrollY > 100);
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const buttonStyle = {
+    position: "fixed",
+    bottom: "2rem",
+    right: "2rem",
+    color: "white",
+    border: "1.5px solid #f5f5f5",
+    borderRadius: "50%",
+    width: "2rem",
+    height: "2rem",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    transition: "opacity 0.3s ease-in-out",
+    fontSize: "1rem",
+    opacity: isVisible ? 1 : 0,
+    pointerEvents: isVisible ? "auto" : "none",
+  };
+
+  return (
+    <button
+      style={buttonStyle}
+      onClick={scrollToTop}
+      aria-label="Scroll to top"
+    >
+      <svg
+        class="w-6 h-6 text-gray-800 dark:text-white"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 6v13m0-13 4 4m-4-4-4 4"
+        />
+      </svg>
+    </button>
+  );
+};
+
+export default ScrollToTop;
