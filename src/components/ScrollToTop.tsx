beb05bffde0644c907f506b7786865c378921368
Luis Torres <torrescereno@gmail.com>
2024-11-10 22:20:59 -0300

    fix styles

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -41,7 +41,7 @@ const ScrollToTop = () => {
       aria-label="Scroll to top"
     >
       <svg
-        class="w-6 h-6 text-gray-800 dark:text-white"
+        class="w-6 h-6"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         width="24"
